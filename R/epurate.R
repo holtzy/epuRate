38aa5a79daf384524629f80037e577e6783f3184
Holtz Yan <yan.holtz.data@gmail.com>
2018-01-19 12:14:20 +1000

    Change function

--- R/epurate.R
+++ R/epurate.R
@@ -1,4 +1,4 @@
-epurate <- function() {
+epurate <- function(toc = TRUE) {
   
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "epurate" ,"resources", "style.css", package = "epuRate")
@@ -7,13 +7,16 @@ epurate <- function() {
   # call the base html_document function
   rmarkdown::html_document( theme= lumen,
                                css= css,
-                               toc= false,
-                               toc_float= true,
+                               toc= toc,
+                               toc_float= TRUE,
                                toc_depth= 2,
-                               number_sections= true,
-                               df_print= paged,
-                               code_folding= hide,
+                               number_sections= TRUE,
                                includes = includes(after_body = footer)
                             )
 
-  }
+}
+
+
+
+#df_print= paged,
+#code_folding= hide,
