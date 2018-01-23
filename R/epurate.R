96c3ceb5a2415dc5fa42421eaf93f761d42136de
Holtz Yan <yan.holtz.data@gmail.com>
2018-01-23 16:11:57 +1000

    Add number paging option

--- R/epurate.R
+++ R/epurate.R
@@ -1,18 +1,18 @@
 library(rmarkdown)
 
-epurate <- function(toc = TRUE, code_folding = "hide") {
-  
+epurate <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "epurate" ,"resources", "style.css", package = "epuRate")
   footer <- system.file("rmarkdown", "templates", "epurate" ,"resources", "footer.html", package = "epuRate")
-  
+
   # call the base html_document function
   rmarkdown::html_document( theme= "lumen",
                                css= css,
                                toc= toc,
                                toc_float= TRUE,
                                toc_depth= 2,
-                               number_sections= TRUE,
+                               number_sections= number_sections,
                                df_print = "paged",
                                code_folding = code_folding,
                                includes = includes(after_body = footer)
