311c44409d1e82952d93a5f47f18406be30546ad
Holtz Yan <yan.holtz.data@gmail.com>
2018-01-23 17:15:03 +1000

    Add GitHub ribbon

--- R/epurate.R
+++ R/epurate.R
@@ -5,6 +5,7 @@ epurate <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "epurate" ,"resources", "style.css", package = "epuRate")
   footer <- system.file("rmarkdown", "templates", "epurate" ,"resources", "footer.html", package = "epuRate")
+  header <- system.file("rmarkdown", "templates", "epurate" ,"resources", "header.html", package = "epuRate")
 
   # call the base html_document function
   rmarkdown::html_document( theme= "lumen",
@@ -15,7 +16,7 @@ epurate <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                number_sections= number_sections,
                                df_print = "paged",
                                code_folding = code_folding,
-                               includes = includes(after_body = footer)
+                               includes = includes(after_body = footer, before_body = header)
                             )
 
 }
