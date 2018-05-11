3a4495d0254ecada1f798557b352db4302e960ed
Holtz Yan <yan.holtz.data@gmail.com>
2018-05-11 15:19:05 +1000

    User can put its name

--- R/epurate.R
+++ R/epurate.R
@@ -4,11 +4,12 @@ epurate <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
 
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "epurate" ,"resources", "style.css", package = "epuRate")
-  footer <- system.file("rmarkdown", "templates", "epurate" ,"resources", "footer.html", package = "epuRate")
   header <- system.file("rmarkdown", "templates", "epurate" ,"resources", "header.html", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "epurate" ,"resources", "template_epurate.html", package = "epuRate")
 
   # call the base html_document function
   rmarkdown::html_document( theme= "lumen",
+                               template= template,
                                css= css,
                                toc= toc,
                                toc_float= TRUE,
@@ -16,7 +17,7 @@ epurate <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                number_sections= number_sections,
                                df_print = "paged",
                                code_folding = code_folding,
-                               includes = includes(after_body = footer, before_body = header)
+                               includes = includes(before_body = header)
                             )
 }
 
