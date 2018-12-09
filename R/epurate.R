b831fed9559908b01c7c52d1a22c3ea08e77b91d
Holtz Yan <yan.holtz.data@gmail.com>
2018-12-10 09:18:02 +1000

    Add qimr template

--- R/epurate.R
+++ R/epurate.R
@@ -63,3 +63,24 @@ UQ <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                code_folding = code_folding,
                             )
 }
+
+
+# Features of QIMR Template
+QIMR <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "QIMR" ,"resources", "style.css", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "QIMR" ,"resources", "template_qimr.html", package = "epuRate")
+
+  # call the base html_document function
+  rmarkdown::html_document( theme= "lumen",
+                               template= template,
+                               css= css,
+                               toc= toc,
+                               toc_float = TRUE,
+                               toc_depth = 2,
+                               number_sections= number_sections,
+                               df_print = "paged",
+                               code_folding = code_folding,
+                            )
+}
