38799d0b61c7838ade760499111eb2b61931fdd4
Holtz Yan <yan1166@hotmail.com>
2018-12-09 12:54:28 +1000

    Epurate almost OK

--- build.R
+++ build.R
@@ -11,7 +11,3 @@ devtools::install("../epuRate")
 
 # Load to check it is all good
 library(epuRate)
-
-# Run one of the skeleton to make sure it works?
-library(rmarkdown)
-render("inst/rmarkdown/templates/epurate/skeleton/skeleton.html")
