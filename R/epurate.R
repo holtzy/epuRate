c5354eb5ac695fe5a73910f0f959931fcabdc380
Holtz Yan <yan1166@hotmail.com>
2018-12-09 13:38:32 +1000

    Add logos

--- R/epurate.R
+++ R/epurate.R
@@ -26,18 +26,17 @@ PCTG <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
 
   # get the locations of resource files located within the package
   css <- system.file("rmarkdown", "templates", "PCTG" ,"resources", "style.css", package = "epuRate")
-  footer <- system.file("rmarkdown", "templates", "PCTG" ,"resources", "footer.html", package = "epuRate")
-  header <- system.file("rmarkdown", "templates", "PCTG" ,"resources", "header.html", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "PCTG" ,"resources", "template_epurate.html", package = "epuRate")
 
   # call the base html_document function
   rmarkdown::html_document( theme= "lumen",
-                            css= css,
-                            toc= toc,
-                            toc_float= TRUE,
-                            toc_depth= 2,
-                            number_sections= number_sections,
-                            df_print = "paged",
-                            code_folding = code_folding,
-                            includes = includes(after_body = footer)
-  )
+                               template= template,
+                               css= css,
+                               toc= toc,
+                               toc_float = TRUE,
+                               toc_depth = 2,
+                               number_sections= number_sections,
+                               df_print = "paged",
+                               code_folding = code_folding,
+                            )
 }
