287d30e09bc9c4c20947c3c284b8c290c189bb6b
Holtz Yan <yan1166@hotmail.com>
2018-12-09 14:24:16 +1000

    Small

--- R/epurate.R
+++ R/epurate.R
@@ -40,3 +40,26 @@ PCTG <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                code_folding = code_folding,
                             )
 }
+
+
+
+
+# Features of PCTG Template
+UQ <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "UQ" ,"resources", "style.css", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "UQ" ,"resources", "template_epurate.html", package = "epuRate")
+
+  # call the base html_document function
+  rmarkdown::html_document( theme= "lumen",
+                               template= template,
+                               css= css,
+                               toc= toc,
+                               toc_float = TRUE,
+                               toc_depth = 2,
+                               number_sections= number_sections,
+                               df_print = "paged",
+                               code_folding = code_folding,
+                            )
+}
