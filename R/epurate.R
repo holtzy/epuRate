e8ce91c478224c807aaa30e2840d91c9b4598c0f
Camilo Garcia <cgarci39@eafit.edu.co>
2020-07-15 10:29:57 -0500

    Adding EAFIT features in the main epurate.R code

--- R/epurate.R
+++ R/epurate.R
@@ -84,3 +84,25 @@ QIMR <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                code_folding = code_folding,
                             )
 }
+
+# Features of EAFIT Template
+EAFIT <- function(toc = TRUE, code_folding = "show", number_sections = TRUE) {
+
+  # get the locations of resource files located within the package
+  css <- system.file("rmarkdown", "templates", "EAFIT" ,"resources", "style.css", package = "epuRate")
+  template <- system.file("rmarkdown", "templates", "EAFIT" ,"resources", "template_eafit.html", package = "epuRate")
+  bibliography <- system.file("rmarkdown", "templates", "EAFIT" ,"resources", "bibliography.bib", package = "epuRate")
+
+  # call the base html_document function
+  rmarkdown::html_document( theme= "lumen",
+                            template = template,
+                            css = css,
+                            bibliography = bibliography,
+                            toc = toc,
+                            toc_float = TRUE,
+                            toc_depth = 2,
+                            number_sections = number_sections,
+                            df_print = "paged",
+                            code_folding = code_folding,
+  )
+}
