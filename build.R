6d9e2874688b9b5f67b30aea6fea34ce853f0fa3
Baptiste Couvy-Duchesne <baptiste.couvyduchesne@gmail.com>
2022-06-01 09:55:51 +0200

    correct path to logo
    now path to github image

--- build.R
+++ build.R
@@ -9,5 +9,10 @@ remove.packages("epuRate")
 library(devtools)
 devtools::install("../epuRate")
 
+# Install from github
+library(devtools)
+devtools::install_github("baptisteCD/epuRate")
+
+
 # Load to check it is all good
 library(epuRate)
